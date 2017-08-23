77ca4eead662a75adbc1acf2618cd45c02cfa1f0
MordogFP <mordog175@gmail.com>
2017-08-23 17:54:05 +0300

    Added kanban board components

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import KanbanBoard from "./KanbanBoard";
+
+let cardList = [
+    {
+        id: 1,
+        title: "Read the Book",
+        description: "I should read the whole book",
+        status: "in-progress",
+        tasks: []
+    },
+    {
+        id: 2,
+        title: "Write some code",
+        description: "Code along with the samples in the book",
+        status: "todo",
+        tasks: [
+            {
+                id: 1,
+                name: "ContactList Example",
+                done: true
+            },
+            {
+                id: 2,
+                name: "Kanban Example",
+                done: false
+            },
+            {
+                id: 3,
+                name: "My own experiments",
+                done: false
+            }
+        ]
+    }
+];
 
 export default class App extends React.Component {
-  render() {
-    return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-      </div>);
-  }
+    render() {
+        return (
+            <KanbanBoard cards={cardList} />);
+    }
 }
